8891658fc10d746295764c59f7407e90fcc5d8b1
sathvik1709 <sathvikshivaprakash519@gmail.com>
2015-04-21 15:01:18 -0500

    delete meeting option added

--- app/src/main/java/com/cse5306/wemeet/fragments/HostMeetingFragment.java
+++ app/src/main/java/com/cse5306/wemeet/fragments/HostMeetingFragment.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -31,6 +33,21 @@ public class HostMeetingFragment extends Fragment {
     ListView hostMeetingListView;
 
     @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getActivity().getMenuInflater().inflate(R.menu.long_press_action,menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        if(item.getItemId() == R.id.delete_action)
+            Toast.makeText(getActivity(),meetingsYouHost.get(info.position).getMeetingDate(),Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(
@@ -43,6 +60,7 @@ public class HostMeetingFragment extends Fragment {
         hostMeetingListView = (ListView) rootView.findViewById(R.id.host_meetings_list);
         mHostMeetingsProgress = (LinearLayout) rootView.findViewById(R.id.host_meetings_lin_layout_progress);
         mHostMeetingsNoMeetings= (LinearLayout) rootView.findViewById(R.id.host_meetings_lin_layout_no_meetings);
+        registerForContextMenu(hostMeetingListView);
 
         // get user host meeting list
         for(int i=0;i<meetingDetailsList.size();i++){
@@ -76,6 +94,15 @@ public class HostMeetingFragment extends Fragment {
 
             }
         });
+
+
+
+        /*hostMeetingListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                return true;
+            }
+        });*/
 
         return rootView;
     }
